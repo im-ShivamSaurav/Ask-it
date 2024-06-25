68e4d2279180dfd0ebdca220d211249e7b14d01f
Shivam Saurav <shivamsaurav63@gmail.com>
2024-06-26 03:42:46 +0530

    Created a ready to deploy app for vercel

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Authnav from "@/components/auth/Authnav.jsx";
 import Authlogin from "@/components/auth/Authlogin.jsx";
-import UserExistsRedirection from "@/app/userExistsRedirection";
+import UserExistsRedirection from "@/app/userExistsRedirection.jsx";
 
 const login = () => {
   return (
