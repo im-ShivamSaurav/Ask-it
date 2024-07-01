e1128f85e9919fa217f3257b7ceb2849b8e86c63
Shivam Saurav <shivamsaurav63@gmail.com>
2024-07-02 04:40:27 +0530

    updated title

---
+++ src/utils/siteConfig.js
@@ -0,0 +1,63 @@
+export const siteConfig = {
+  icons: {
+    shortcut: ["/favicon.ico"],
+    icon: [
+      { url: "/favicon.ico", type: "image/x-icon" },
+      { url: "/icons/favicon-16x16.png", type: "image/png", sizes: "16x16" },
+      { url: "/icons/favicon-32x32.png", type: "image/png", sizes: "32x32" },
+      //   { url: "/icons/favicon-96x96.png", type: "image/png", sizes: "96x96" },
+      {
+        url: "/icons/favicon-196x196.png",
+        type: "image/png",
+        sizes: "196x196",
+      },
+    ],
+    apple: [
+      {
+        url: "/icons/apple-touch-icon-57x57.png",
+        sizes: "57x57",
+        type: "image/png",
+      },
+      {
+        url: "/icons/apple-touch-icon-60x60.png",
+        sizes: "60x60",
+        type: "image/png",
+      },
+      {
+        url: "/icons/apple-touch-icon-72x72.png",
+        sizes: "72x72",
+        type: "image/png",
+      },
+      {
+        url: "/icons/apple-touch-icon-76x76.png",
+        sizes: "76x76",
+        type: "image/png",
+      },
+      {
+        url: "/icons/apple-touch-icon-114x114.png",
+        sizes: "114x114",
+        type: "image/png",
+      },
+      {
+        url: "/icons/apple-touch-icon-120x120.png",
+        sizes: "120x120",
+        type: "image/png",
+      },
+      {
+        url: "/icons/apple-touch-icon-144x144.png",
+        sizes: "144x144",
+        type: "image/png",
+      },
+      {
+        url: "/icons/apple-touch-icon-152x152.png",
+        sizes: "152x152",
+        type: "image/png",
+      },
+      {
+        url: "/icons/apple-touch-icon-180x180.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  },
+};
